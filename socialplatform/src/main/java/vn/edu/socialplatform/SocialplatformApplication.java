61c2e6755cbab1f7b7c604f6c5e1f9251a95a2d3
TaAnhKiet <18130119@st.hcmuaf.edu.vn>
2022-05-05 20:22:23 +0700

    Create backend Spring boot for project

---
+++ socialplatform/src/main/java/vn/edu/socialplatform/SocialplatformApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.socialplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialplatformApplication.class, args);
+	}
+
+}
